0b11c691c77c3ea4790672036bf1bf10e2d5a854
josenelle <josenellesantos@gmail.com>
2021-05-05 22:26:17 -0300

    componente Tag

--- src/components/Tag/index.js
+++ src/components/Tag/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./styles.css";
 const Tag = props => (
-    <button className="tag">
+    <button
+        className={`tag ${props.selectedTag === props.children ? "active" : ""}`}
+        onClick={props.handleTags(props.children)}
+    >
         {props.children}
     </button>
 );
